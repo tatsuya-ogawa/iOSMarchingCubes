13783a04d75d0193cf37d4a17ffebae6180f7dc6
tatsuya ogawa <togawa83@gmail.com>
2023-04-09 22:35:00 +0900

    Initial Commit

---
+++ iOSMarchingCube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSMarchingCube
+//
+//  Created by Tatsuya Ogawa on 2023/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
